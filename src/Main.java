0e64849dbd836f56524875065d5eef47ff6b53d0
Ramil-Karipov <omil8@yandex.ru>
2023-10-09 17:42:26 +0500

    task1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 30;
+        if (age>=18) {
+            System.out.println("Ты достиг совершеннолетия");
+        }
+        if (age<18) {
+            System.out.println("Ты достиг совершеннолетия");
+        }
+
+    }
+}
