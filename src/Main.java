488a5c8fe7a17045236c7b070c3358464669af9b
Ramil-Karipov <omil8@yandex.ru>
2023-10-10 11:21:03 +0500

    task3

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ public class Main {
         System.out.println("Задание 1");
         int age = 10;
         if (age>=18) {
-            System.out.println("Ты достиг совершеннолетия");
+            System.out.println("Если тебе " + age + " ты достиг совершеннолетия");
         }
         if (age<18) {
-            System.out.println("Ты не достиг совершеннолетия, нужно немного подождать");
+            System.out.println("Если тебе " + age + " ты не достиг совершеннолетия, нужно немного подождать");
         }
         System.out.println("Задание 2");
         int temperature = 3;
@@ -15,6 +15,16 @@ public class Main {
         } else {
             System.out.println("На улице холодно, нужно надеть шапку");
         }
+        System.out.println("Задание 3");
+        int speed = 50;
+        if (speed > 60) {
+            System.out.println("Если скорость " + speed + " ,то прийдется заплатить штраф");
+        }
+        if (speed <= 60) {
+            System.out.println("Если скорость " + speed + " ,то можно ездить спокойно");
+        }
+    }
+
+
 
     }
-}
