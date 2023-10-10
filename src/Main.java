0e96820b7caffefdb2c9741bf7009d807c1da22a
Ramil-Karipov <omil8@yandex.ru>
2023-10-10 10:45:42 +0500

    task2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 30;
+        System.out.println("Задание 1");
+        int age = 10;
         if (age>=18) {
             System.out.println("Ты достиг совершеннолетия");
         }
         if (age<18) {
-            System.out.println("Ты достиг совершеннолетия");
+            System.out.println("Ты не достиг совершеннолетия, нужно немного подождать");
+        }
+        System.out.println("Задание 2");
+        int temperature = 3;
+        if (temperature > 5) {
+            System.out.println("Сегодня тепло, можно идти без шапки");
+        } else {
+            System.out.println("На улице холодно, нужно надеть шапку");
         }
 
     }
